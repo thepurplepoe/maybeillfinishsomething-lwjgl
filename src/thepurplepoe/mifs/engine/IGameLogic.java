d15c5a5697b0014c688a7a2d029a63cc8c69c563
thepurplepoe <thepurplepoe.um@gmail.com>
2017-07-06 23:32:34 +1200

    Fixed gitignore, added base code

---
+++ src/thepurplepoe/mifs/engine/IGameLogic.java
@@ -0,0 +1,12 @@
+package thepurplepoe.mifs.engine;
+
+public interface IGameLogic {
+
+    void init() throws Exception;
+
+    void input(Window window);
+
+    void update(float interval);
+    
+    void render(Window window);
+}
---
+++ src/thepurplepoe/mifs/game/Main.java
@@ -0,0 +1,19 @@
+package thepurplepoe.mifs.game;
+
+import thepurplepoe.mifs.engine.GameEngine;
+import thepurplepoe.mifs.engine.IGameLogic;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        try {
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+            gameEng.start();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
