bb54f3b55ec27a6393da4c09a1e241328b5ace28
thepurplepoe <thepurplepoe.um@gmail.com>
2017-07-07 22:04:52 +1200

    Mems

--- src/thepurplepoe/mifs/engine/GameEngine.java
+++ src/thepurplepoe/mifs/engine/GameEngine.java
@@ -2,7 +2,7 @@ package thepurplepoe.mifs.engine;
 
 public class GameEngine implements Runnable {
 
-    public static final int TARGET_FPS = 75;
+    public static final int TARGET_FPS = 60;
 
     public static final int TARGET_UPS = 30;
 
